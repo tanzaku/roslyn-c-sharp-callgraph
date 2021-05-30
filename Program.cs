aba0b99faa651ca86acac7088632919900c241be
tanzaku <tanzaku@users.noreply.github.com>
2021-05-30 14:50:40 +0900

    new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace roslyn_c_sharp_callgraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
